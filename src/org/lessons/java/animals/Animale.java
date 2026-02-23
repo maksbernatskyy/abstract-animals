31fd105dce8ce2ca5ca6390c6f9bc60978835cde
maksbernatskyy <maksbernatskyy@gmail.com>
2026-02-23 10:58:47 +0100

    add methods in Animale

--- src/org/lessons/java/animals/Animale.java
+++ src/org/lessons/java/animals/Animale.java
@@ -3,13 +3,26 @@ package org.lessons.java.animals;
 import java.math.BigDecimal;
 
 public abstract class Animale {
+    // Attributi
     protected String nome;
     protected BigDecimal peso;
     protected String colore;
 
+    // Metodi
     public Animale(String nome, BigDecimal peso, String colore) {
         this.nome = nome;
         this.peso = peso;
         this.colore = colore;
     }
+
+    // Dormire uguale per tutti
+    public void dormi() {
+        System.out.println("Zzz");
+    }
+
+    // Verso diverso
+    public abstract void verso();
+
+    // Cosa mangia
+    public abstract void mangia();
 }
